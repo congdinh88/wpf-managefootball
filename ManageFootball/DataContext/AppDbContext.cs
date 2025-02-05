71826a6359385852f01e5150dcdbe7dbb392c28c
congdinh88 <congdinh.tk@gmail.com>
2025-02-05 16:24:40 +0700

    05/02/25

---
+++ ManageFootball/DataContext/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageFootball.DataContext
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext() : base("name=DefaultConnection")
+        {
+        }
+    }
+}
